445cbf4861c0b8eecd453eb370dea88755008c6b
Tory <vroland51096@gmail.com>
2019-07-19 21:53:39 -0400

    added read me

--- MoveZeroes/MoveZeroes/Program.cs
+++ MoveZeroes/MoveZeroes/Program.cs
@@ -17,5 +17,8 @@ namespace MoveZeroes
 
             Console.ReadLine();
         }
+
+        /* Write an algorithm that takes an array and moves all of the zeros to the end, 
+         * preserving the order of the other elements.*/
     }
 }
