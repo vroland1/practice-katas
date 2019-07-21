326ba2e5d97206f0cf2658729a708a5509f46187
Tory <vroland51096@gmail.com>
2019-07-20 21:37:40 -0400

    changed some file names, added folders for other languages

---
+++ Javascript/BalanceNumbers/BalancedNumber.js
@@ -0,0 +1,10 @@
+function balancedNum(number)
+{
+    let numAsString  = number.toString();
+    
+    if(numAsString.length = 1){
+    return "Balanced'
+    }
+    elseif(numAsString.length % 2
+    
+}
