2fc9b075f027ecca7228201bf2da2eb2ac95994f
Tory <vroland51096@gmail.com>
2019-07-20 21:44:47 -0400

    rearranged misplaced files

--- Javascript/BalanceNumbers/BalancedNumber.js
+++ Javascript/BalanceNumbers/BalancedNumber.js
@@ -2,9 +2,12 @@ function balancedNum(number)
 {
     let numAsString  = number.toString();
     
-    if(numAsString.length = 1){
-    return "Balanced'
+    if(numAsString.length == 1){
+    return "Balanced"
     }
-    elseif(numAsString.length % 2
+    elseif(numAsString.length % 2 == 0){
+        let leftSum = numAsString.substr(0, (numAsString.length/2));
+        leftSum = 
+    }
     
 }
--- MoveZeroes/README.md
+++
@@ -1,3 +0,0 @@
-# Move Zeroes To The End
-
-Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
